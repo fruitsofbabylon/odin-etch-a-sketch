93ea428887c8f357a16969d7469cecacf7fcee53
Anna Orlova <orlovanna412@gmail.com>
2024-01-19 21:52:21 +0000

    add button to change the number of squares in grid

--- javascript.js
+++ javascript.js
@@ -1,21 +1,40 @@
-function makeGrid() {
-    const container = document.createElement('div');
-    document.body.appendChild(container);
-    container.classList.add("container");
+const container = document.createElement('div');
+document.body.appendChild(container);
+container.classList.add('container');
 
-    for (let i = 1; i <= 256; i++) {
+function createGrid(gridNum) {
+    for (let i = 1; i <= gridNum * gridNum; i++) {
         const square = document.createElement('div');
-        square.classList.add("square");
+        square.classList.add('square');
+        square.style.cssText = 'flex-basis: ' + 100 / gridNum + '%;';
         container.appendChild(square);
     }
-};
+    fillSquares();
+}
 
-makeGrid();
+createGrid(16);
+document.body.appendChild(container);
 
-const squares = document.querySelectorAll('.square');
+const btn = document.createElement('button');
+btn.textContent = 'Change Grid'
+document.body.append(btn);
 
-squares.forEach((square) => {
-  square.addEventListener("mouseenter", () => {
-    square.classList.add("color");
-  });
+btn.addEventListener('click', () => {
+    let newGridNum = prompt('Please enter a number of squares per side for the new grid');
+    if (parseInt(newGridNum, 10) && newGridNum <= 100 && newGridNum > 0) {
+        container.replaceChildren();
+        createGrid(newGridNum);
+    } else {
+        alert('Please use any positive number less than 100');
+    }
 });
+
+function fillSquares() {
+    const squares = document.querySelectorAll('.square');
+
+    squares.forEach((square) => {
+        square.addEventListener('mouseenter', () => {
+            square.classList.add('color');
+        });
+    });
+};
